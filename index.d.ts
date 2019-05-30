07ed28eaf891e0bf4ab487ab1b9261cb658c83a5
Ian Howe <ian@vbizapp.com>
2019-05-30 16:50:20 +1000

    feat(typescript): Allow input to be options or string

--- index.d.ts
+++ index.d.ts
@@ -15,7 +15,7 @@ declare module 'https-proxy-agent' {
     
     // HttpsProxyAgent doesnt *actually* extend https.Agent, but for my purposes I want it to pretend that it does
     class HttpsProxyAgent extends https.Agent {
-        constructor(opts: HttpsProxyAgent.HttpsProxyAgentOptions)
+        constructor(opts: HttpsProxyAgent.HttpsProxyAgentOptions | string)
     }
 
     export = HttpsProxyAgent
