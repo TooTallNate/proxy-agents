ea6982d548d30d475bc7cf20a4a08b702f88de35
Nathan Rajlich <n@n8.io>
2023-05-25 11:15:46 -0700

    Debugging tags script

--- scripts/pack.mjs
+++ scripts/pack.mjs
@@ -5,8 +5,8 @@ import {
 	writeFileSync,
 	readdirSync,
 	mkdirSync,
-    renameSync,
-} from "fs";
+	renameSync,
+} from 'fs';
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 const { VERCEL_URL } = process.env;
@@ -19,17 +19,17 @@ const packages = readdirSync(join(cwd, '..'));
 const pkgPath = join(cwd, 'package.json');
 const pkg = JSON.parse(readFileSync(pkgPath));
 if (pkg.dependencies) {
-    for (const dep of Object.keys(pkg.dependencies)) {
-        if (packages.includes(dep)) {
-            // This dep is one of the ones within the monorepo,
-            // so update to use a tarball URL
-            pkg.dependencies[dep] = `https://${VERCEL_URL}/${dep}.tgz`
-        }
-    }
+	for (const dep of Object.keys(pkg.dependencies)) {
+		if (packages.includes(dep)) {
+			// This dep is one of the ones within the monorepo,
+			// so update to use a tarball URL
+			pkg.dependencies[dep] = `https://${VERCEL_URL}/${dep}.tgz`;
+		}
+	}
 }
 writeFileSync(pkgPath, `${JSON.stringify(pkg, null, 2)}\n`);
 spawnSync('pnpm', ['pack']);
 
-const tarball = readdirSync(cwd).find(f => f.endsWith('.tgz'));
+const tarball = readdirSync(cwd).find((f) => f.endsWith('.tgz'));
 const dest = new URL(`${pkg.name}.tgz`, publicDir);
 renameSync(join(cwd, tarball), dest);
